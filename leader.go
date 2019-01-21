56786e6552bd4b8176b45ed9e0c587bb1cd7a73a
Jason Berlinsky <jason@barefootcoders.com>
2019-01-21 14:50:04 -0500

    Add leadership elections

---
+++ leader.go
@@ -0,0 +1,42 @@
+package resqueExporter
+
+import (
+	"net/http"
+	"os"
+)
+
+var (
+	leaderElectionDuration = "2m"
+	leading                = false
+)
+
+type leaderElectionResponse struct {
+	Name string
+}
+
+func isLeader() bool {
+	leadershipResponse, err := http.Get("http://localhost:4040")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer leadershipResponse.Body.Close()
+	contents, err := ioutil.ReadAll(leadershipResponse.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	var leaderElection LeaderElectionResponse
+	err = json.Unmarshal(contents, &leaderElection)
+	if err != nil {
+		panic(err.Error())
+	}
+	ourHostname := hostname()
+	return leaderElection.Name == ourHostname
+}
+
+func hostname() string {
+	hostname, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	return hostname
+}
